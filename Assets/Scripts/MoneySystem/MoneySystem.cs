ae8812007b3890d3787fb316017021e805e7d619
Ali Kaner <ali.kaner@hotmail.com>
2022-10-09 15:05:53 +0300

    initial-commit

---
+++ Assets/Scripts/MoneySystem/MoneySystem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneySystem : MonoBehaviour
+{
+    public void IncreaseMoney(int amount)
+    {
+    }
+
+    public void DecreaseMoney(int amount)
+    {
+        
+    }
+}
